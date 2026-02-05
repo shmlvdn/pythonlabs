b40e0f1d28b7ed2e54e12e20db178aa9a4b8c342
shmlvdn <keypop659@gmail.com>
2026-02-05 16:14:44 +0500

    Update 00_distance.py

--- lab1/00_distance.py
+++ lab1/00_distance.py
@@ -12,7 +12,17 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
-distances = {}
+distances = {
+    'Moscow' : {
+        "London":((sites['Moscow'][0] - sites['London'][0]) ** 2 + (sites['Moscow'][1] - sites['London'][1]) ** 2) ** 0.5,
+        "Paris":((sites['Moscow'][0] - sites['Paris'][0]) ** 2 + (sites['Moscow'][1] - sites['Paris'][1]) ** 2) ** 0.5},
+    'London' : {
+        "Moscow":((sites['London'][0] - sites['Moscow'][0]) ** 2 + (sites['London'][1] - sites['Moscow'][1]) ** 2) ** 0.5,
+        "Paris":((sites['London'][0] - sites['Paris'][0])** 2 + (sites['London'][1] - sites['Paris'][1]) ** 2) ** 0.5},
+    'Paris' : {
+        "Moscow":((sites['Paris'][0] - sites['Moscow'][0]) ** 2 + (sites['Paris'][1] - sites['Moscow'][1]) ** 2) ** 0.5,
+        "London":((sites['Paris'][0] - sites['London'][0]) ** 2 + (sites['Paris'][1] - sites['London'][1]) ** 2) ** 0.5}
+    }
 
 # TODO здесь заполнение словаря
 
