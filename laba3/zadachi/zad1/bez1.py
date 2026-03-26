225e4c6aead4806645f1ce3d5826505a5427c1d5
shmlvdn <keypop659@gmail.com>
2026-03-26 15:06:56 +0500

    Update bez1.py

--- laba3/zadachi/zad1/bez1.py
+++ laba3/zadachi/zad1/bez1.py
@@ -1,12 +1,21 @@
 def f(x: list) -> str:
-    if not list:
+    if not x:
         return ""
+    
     result = ""
-    for a in x: 
-        if isinstance(a, list): result = result + f(a)
-        else: result += str(a)
+    stack = list(x)
+    
+    while stack:
+        element = stack.pop()
+        
+        if isinstance(element, list):
+            stack.extend(reversed(element))
+        else:
+            result = str(element) + result
     return result
+
 l0 = [1, 2, 3, 4, 5]
-l1 = [1, 2], [3, [[ 4 ]], 5]
-print(f(l0))
+l1 = [1, [2], [3, [[4]]], 5]
+
+print(f(l0)) 
 print(f(l1))
