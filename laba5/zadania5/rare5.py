a4cd2d6b95a9dadea2c8546780da22cf239438a6
shmlvdn <keypop659@gmail.com>
2026-04-09 15:59:05 +0500

    Update rare5.py

--- laba5/zadania5/rare5.py
+++ laba5/zadania5/rare5.py
@@ -4,9 +4,4 @@ numbers = map(int, pi)
 non_zero = filter(lambda x: x != 0, numbers)
 vychisl = map(lambda x: 1 / x, non_zero) 
 result = sum(vychisl)
-
-zeros = list(filter(lambda x: x == 0, map(int, pi)))
-for n in zeros:
-    print("0")
-
 print(result)
