584c3c7672a00b5733cb740f33bd4a5d3d5bcfdd
shmlvdn <keypop659@gmail.com>
2026-04-15 21:29:39 +0500

    Update rare5.py

--- laba5/zadania5/rare5.py
+++ laba5/zadania5/rare5.py
@@ -1,7 +1,11 @@
-pi = "31415926535897932384"
+def pi_generator():
+    pi_digits = "31415926535897932384"
+    for digit in pi_digits:
+        yield int(digit)
 
-numbers = map(int, pi)
-non_zero = filter(lambda x: x != 0, numbers)
-vychisl = map(lambda x: 1 / x, non_zero) 
+digits = list(pi_generator())
+non_zero = filter(lambda x: x != 0, digits)
+vychisl = map(lambda x: 1 / x, non_zero)
 result = sum(vychisl)
+
 print(result)
